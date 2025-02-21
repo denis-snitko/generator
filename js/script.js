844fe4e38d0ba199f87c223428422cfcc6e2a31c
Denis Snitko <denissnitko@gmail.com>
2025-02-21 20:18:37 +0000

    add MAX_VALUE

--- js/script.js
+++ js/script.js
@@ -3,17 +3,22 @@ document.addEventListener('DOMContentLoaded', function () {
   const maxValueInput = document.getElementById('maxValue');
   const generateBtn = document.getElementById('generateBtn');
   const resultSpan = document.getElementById('result');
+  const MAX_VALUE = 10000000; // Максимальное значение
 
-  function removeLeadingZeros(input) {
+  function sanitizeInput(input) {
     input.value = input.value.replace(/^0+/, '') || '0';
+
+    if (parseInt(input.value, 10) > MAX_VALUE) {
+      input.value = MAX_VALUE;
+    }
   }
 
   minValueInput.addEventListener('input', function () {
-    removeLeadingZeros(this);
+    sanitizeInput(this);
   });
 
   maxValueInput.addEventListener('input', function () {
-    removeLeadingZeros(this);
+    sanitizeInput(this);
   });
 
   generateBtn.addEventListener('click', function () {
